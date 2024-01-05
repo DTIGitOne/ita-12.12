528fae4b5648029edf387d77d0a663039d4a4cde
DTIGitOne <dammirtaljanovic@gmail.com>
2024-01-05 20:18:38 +0100

    feat:small tweaks

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 let root = document.getElementById("root");
 let body = document.getElementById("body");
 
+let size;
 root.addEventListener('click' , function() {
    
-   let size = randomSize();
 
    function getRandomColor2() {
       let letters = '0123456789ABCDEF';
@@ -21,58 +21,9 @@ root.addEventListener('click' , function() {
    ballRandom.className = "ballRandom";
    ballRandom.style.backgroundColor = randomBackground;
    document.body.prepend(ballRandom);
-   
 
-
-
-   ballRandom.addEventListener('mouseover' , function() {
-      const intervalId = setInterval(function() {
-         size += 1;
-         ballRandom.style.width = size + "px";
-         ballRandom.style.height = size + "px";
-         
-         ballRandom.addEventListener('mouseleave' , function() {
-            clearInterval(intervalId);
-         });
-   
-         if (size >= 1745) {
-            clearInterval(intervalId);
-         }
-      }, 10);
-      }
-   );
-
-
-   function randomSize () {
-      let sizeScale = "1234567890000000";
-      let number = '';
-      let overValue = 700;
-      for (let i=0 ; i <=2 ; i++) {
-         let randomIndex = Math.floor(Math.random() * sizeScale.length);
-         number += sizeScale[randomIndex];
-         if ( number > overValue) {
-            number -= 300;
-         }
-      }
-      return number + 'px';
-   }
-
-   function randomHeight () {
-      let sizeScale = "1234567890000";
-      let number = '';
-      let overValue = 866;
-      for (let i=0 ; i <=2 ; i++) {
-         let randomIndex = Math.floor(Math.random() * sizeScale.length);
-         number += sizeScale[randomIndex];
-         if ( number > overValue) {
-            number -= 300;
-         }
-      }
-      return number + 'px';
-   }
-   
-   function randomForRandom() {
-      let sizeScale = "23"
+   function randomFor4() {
+      let sizeScale = "123456"
       let number = '';
       for (let i=0 ; i <1 ; i++) {
          let randomIndex = Math.floor(Math.random() * sizeScale.length);
@@ -80,30 +31,128 @@ root.addEventListener('click' , function() {
       }
       return parseInt(number);
    }
-   
-   function randomSideWidth () {
-      let sizeScale = "1234567890000";
+
+   function randomForRandom4() {
+      let sizeScale = "123456789"
       let number = '';
-      let overValue = 1745;
-      for (let i=0 ; i <= randomForRandom() ; i++) {
+      for (let i=0 ; i < randomFor4()  ; i++) {
+         let randomIndex = Math.floor(Math.random() * sizeScale.length);
+         number += sizeScale[randomIndex];
+      }
+      return parseInt(number);
+   }
+  
+  let rnNumber = randomForRandom4();
+  
+  ballRandom.style.zIndex = rnNumber;
+
+
+
+   function randomSize () {
+      let sizeScale = "1234567890000000";
+      let number = '';
+      let overValue = 500;
+      for (let i=0 ; i <=2 ; i++) {
          let randomIndex = Math.floor(Math.random() * sizeScale.length);
          number += sizeScale[randomIndex];
          if ( number > overValue) {
-            number = 1745 - randomHeight();
+            number -= 300;
          }
       }
       return number + 'px';
    }
 
+   
+   function removeZero(v) {
+      if (typeof v === 'string' && v.charAt(0) === '0') {
+        return v.slice(1);
+      }
+      return v;
+    }
+
+    function randomHeight() {
+      let sizeScale = "1234567890";
+      let number = '';
+      let overValue = 953;
+      for (let i = 0; i <= 2; i++) {
+        let randomIndex = Math.floor(Math.random() * sizeScale.length);
+        number += sizeScale[randomIndex];
+        if (number > overValue) {
+          number -= 1;
+        }
+      }
+      number = removeZero(number);
+      return number + 'px';
+    }
+   
+   function randomForRandom() {
+      let sizeScale = "23";
+      let number = '';
+      for (let i = 0; i < 1; i++) {
+        let randomIndex = Math.floor(Math.random() * sizeScale.length);
+        number += sizeScale[randomIndex];
+      }
+      return parseInt(number);
+    }
+    
+    
+    
+    function randomSideWidth() {
+      let sizeScale = "12345678900";
+      let number = '';
+      let overValue = 1920;
+      for (let i = 0; i <= randomForRandom(); i++) {
+        let randomIndex = Math.floor(Math.random() * sizeScale.length);
+        number += sizeScale[randomIndex];
+        if (number > overValue) {
+         let heightValue = parseFloat(randomHeight()) || 0;
+         number = 1920 - heightValue;
+        }
+        number = removeZero(number);
+      }
+      return number + 'px';
+    }
+
    let z = randomSize();
    
    let y = randomHeight();
-   
+  
    let j = randomSideWidth();
+
    
+   function randomForRandom2() {
+      let sizeScale = "01"
+      let number = '';
+      for (let i=0 ; i <1 ; i++) {
+         let randomIndex = Math.floor(Math.random() * sizeScale.length);
+         number += sizeScale[randomIndex];
+      }
+      return parseInt(number);
+   }
+
+   let f = randomForRandom2();
    
-   ballRandom.style.left = j;
-   ballRandom.style.top = y;
+   let sideChoose = ["left" , "right"];
+   let choose1 = sideChoose[f];
+
+   let bgRandom = choose1;
+   
+   let upDownChoose = ["top" , "bottom"];
+   let choose2 = upDownChoose[f];
+
+   let upDownRandom = choose2;
+  
+   if (bgRandom === "left") {
+      ballRandom.style.left = j;
+   } else if ( bgRandom === "right") {
+      ballRandom.style.right = j;
+   }
+   if (upDownRandom === "top") {
+      ballRandom.style.top = y;
+   } else if (upDownRandom === "bottom") {
+      ballRandom.style.bottom = y;
+   }
+
    ballRandom.style.width = z;
    ballRandom.style.height = z;
 
@@ -124,24 +173,41 @@ root.addEventListener('click' , function() {
    });
 
    
-
+   ballRandom.addEventListener('mouseover', function() {
+      const intervalId = setInterval(function() {
+        size += 1;
+        ballRandom.style.width = size ;
+        ballRandom.style.height = size ;
+        
+        if (size >= 1745) {
+          clearInterval(intervalId);
+        }
+      }, 10);
+      
+      ballRandom.addEventListener('mouseleave', function() {
+         clearInterval(intervalId);
+       });
+     });
 
   }
 
   
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+ballRandom.addEventListener('mouseover', function() {
+      const intervalId = setInterval(function() {
+        size += 1;
+        ballRandom.style.width = size + "px";
+        ballRandom.style.height = size + "px";
+        
+        if (size >= 1745) {
+          clearInterval(intervalId);
+        }
+      }, 10);
+      
+      ballRandom.addEventListener('mouseleave', function() {
+         clearInterval(intervalId);
+       });
+     });
+*/
